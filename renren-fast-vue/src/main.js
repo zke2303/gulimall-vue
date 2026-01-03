b3a18ec0577202a896b27f153b0f9c41a99d4172
nanfeng <nanfeng.dev@gmail.com>
2026-01-03 11:04:10 +0800

    feat(gatewary): 将前端请求都转发都gatewary中

--- renren-fast-vue/src/main.js
+++ renren-fast-vue/src/main.js
@@ -1,15 +1,16 @@
-import Vue from 'vue'
 import App from '@/App'
-import router from '@/router'                 // api: https://github.com/vuejs/vue-router
-import store from '@/store'                   // api: https://github.com/vuejs/vuex
-import VueCookie from 'vue-cookie'            // api: https://github.com/alfhen/vue-cookie
-import '@/element-ui'                         // api: https://github.com/ElemeFE/element
-import '@/icons'                              // api: http://www.iconfont.cn/
-import '@/element-ui-theme'
 import '@/assets/scss/index.scss'
-import httpRequest from '@/utils/httpRequest' // api: https://github.com/axios/axios
+import '@/element-ui'; // api: https://github.com/ElemeFE/element
+import '@/element-ui-theme'
+import '@/icons'; // api: http://www.iconfont.cn/
+import router from '@/router'; // api: https://github.com/vuejs/vue-router
+import store from '@/store'; // api: https://github.com/vuejs/vuex
 import { isAuth } from '@/utils'
+import httpRequest from '@/utils/httpRequest'; // api: https://github.com/axios/axios
 import cloneDeep from 'lodash/cloneDeep'
+import Vue from 'vue'
+import VueCookie from 'vue-cookie'; // api: https://github.com/alfhen/vue-cookie
+
 
 Vue.use(VueCookie)
 Vue.config.productionTip = false
---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-tree
+    :data="data"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+  ></el-tree>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      data: [],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getMenus () {
+        this.$http({
+            url: this.$http.adornUrl('/product/category/list/tree'),
+            method: 'get',
+        }).then((res) => {
+            console.log(res)
+        })
+    }
+  },
+  mounted () {
+    this.getMenus()
+  }
+}
+</script>
